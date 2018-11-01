2bd9997d695d95a8bc73960acc63530fdf2226b3
KarinaTiurina <rktiurina@gmail.com>
2018-11-01 22:45:28 +0300

    class Calculator

--- app1/index.js
+++ app1/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import Calculator from '~/src/calculator';
+
+const calc = new Calculator();
+
+console.log(calc.plus(7, 4));
+console.log(calc.minus(7, 4));
+console.log(calc.multiply(7, 4));
+console.log(calc.devide(7, 4));
---
+++ app1/src/calculator.js
@@ -0,0 +1,10 @@
+class Calculator {
+  constructor() {
+    this.plus = (a, b) => (a + b);
+    this.minus = (a, b) => (a - b);
+    this.multiply = (a, b) => (a * b);
+    this.devide = (a, b) => (a / b);
+  }
+}
+
+export default Calculator;
